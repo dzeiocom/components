be01f386d4ca9611e1d308515ea979222963525f
Florian BOUILLON <florian.bouillon@delta-wings.net>
2021-05-18 11:09:58 +0200

    Added Overflow X to table element
    Signed-off-by: Florian BOUILLON <florian.bouillon@delta-wings.net>

--- src/dzeio/Table/index.tsx
+++ src/dzeio/Table/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { buildClassName } from '../Util'
 
 import css from './Table.module.styl'
 
 interface Props {
 	children: React.ReactNode
+	parentClassName?: string
+	className?: string
 }
 
 export default class Table extends React.Component<Props> {
 
 	public render = () => (
-		<table className={css.table}>{this.props.children}</table>
+		<div className={buildClassName(css.parent, this.props.parentClassName)}>
+			<table className={buildClassName(css.table, this.props.className)}>{this.props.children}</table>
+		</div>
 	)
 
 }
